17bc901d9d360aa02f6a456617f34148e31802f1
Samir Al-Bulushi <albulushii.samir@gmail.com>
2025-03-24 07:21:44 +0400

    Adding comments

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace SimpleStudentManagementProject_CSharpProject1
 {
     internal class Program
     {
+        //Declearing Arrays
         static double[] marks = new double[10];
         static int[] Ages = new int[10];
         static string[] names = new string[10];
@@ -15,6 +16,7 @@ namespace SimpleStudentManagementProject_CSharpProject1
             while (true)
             {
                 Console.Clear();
+                //Menu Options
                 Console.WriteLine("\nChoose an Array Exercise:");
                 Console.WriteLine("1. Add a new student record (Name, Age, Marks) ");
                 Console.WriteLine("2. View all students");
@@ -48,23 +50,27 @@ namespace SimpleStudentManagementProject_CSharpProject1
         }
 
         /* -------------------------- 1. Add New Student  -------------------*/
+        
         static void AddNewStudent()
         {
+            //Declearing Variables
             string StudentName;
             int StudentAge = 0, StudentAge1 = 0;
             double StudentMark = 0;
             DateTime StudentEnDate = DateTime.Now;
 
+            //Checking if the array is full
             if (StudentCounter == 10)
             {
                 Console.WriteLine("Array is Full! You can't add more students.");
                 
             }
+            //If the array is not full
             else
             {
                 Console.WriteLine("\n <--------------- Enter Student Data --------------->");
 
-
+                //Getting Student Name
                 for (int i = StudentCounter; i < names.Length; i++)
                 {
                     Console.WriteLine("Enter Student Name: ");
@@ -72,8 +78,11 @@ namespace SimpleStudentManagementProject_CSharpProject1
                     names[i] = StudentName;
                     names[i] = names[i].ToLower();
 
+                    //Getting Student Age
                     Console.WriteLine("Enter Student Age:  ");
                     StudentAge = int.Parse(Console.ReadLine());
+
+                    //Checking if the age is less than 21
                     for (int j = 0; StudentAge < 21; j++)
                     {
                         if (21 > StudentAge)
@@ -91,8 +100,11 @@ namespace SimpleStudentManagementProject_CSharpProject1
 
                     Ages[i] = StudentAge;
 
+                    //Getting Student Enrollment Date
                     Console.WriteLine("Enter Student Mark: ");
                     StudentMark = double.Parse(Console.ReadLine());
+
+                    //Checking if the mark is between 0 and 100
                     for (int j = 0; StudentMark < 0 && StudentMark > 100; j++)
                     {
                         if (21 > StudentAge)
@@ -113,7 +125,7 @@ namespace SimpleStudentManagementProject_CSharpProject1
 
                     dates[i] = StudentEnDate;
 
-                    StudentCounter++;
+                    StudentCounter++; //Incrementing the counter
 
                     Console.WriteLine($"Name of Student: {names[i]} , Student Age: {Ages[i]} , Student Mark: {marks[i]} , Student Enrollment Date: {dates[i]} \n");
 
@@ -139,6 +151,7 @@ namespace SimpleStudentManagementProject_CSharpProject1
         {
             Console.WriteLine("\n*------  List of All Students  ------*\n");
 
+            //Printing all the students
             for (int i = 0; i < StudentCounter; i++)
             {
                 Console.WriteLine($"{i}: Name of Student: {names[i]} , Student Age: {Ages[i]} , Student Mark: {marks[i]} , Student Enrollment Date: {dates[i]} \n");
@@ -152,8 +165,11 @@ namespace SimpleStudentManagementProject_CSharpProject1
 
         static void FindStudentByName()
         {
+            //Declearing Variables
             int index;
             string StudentName;
+
+            //Getting Student Name
             for (int i = 0; i < StudentCounter; i++)
             {
 
@@ -162,7 +178,9 @@ namespace SimpleStudentManagementProject_CSharpProject1
                 StudentName = Console.ReadLine();
                 StudentName = StudentName.ToLower();
 
-                index = Array.IndexOf(names, StudentName);
+                index = Array.IndexOf(names, StudentName); //Finding the index of the student
+
+                //Printing the student details
                 if (index != -1)
                 {
                     Console.WriteLine($"Position: {index} , Name of Student: {names[index]} , Student Age: {Ages[index]} , Student Mark: {marks[index]} , Student Enrollment Date: {dates[index]} \n");
@@ -179,15 +197,18 @@ namespace SimpleStudentManagementProject_CSharpProject1
 
         static void CalculateClassAverage()
         {
-
+            //Declearing Variables
             double SumOfMarks = 0;
             double ClassAverage = 0;
+
+            //Calculating the class average
             for (int i = 0; i < StudentCounter; i++)
             {
                 SumOfMarks += marks[i];
             }
-            ClassAverage = SumOfMarks / StudentCounter;
-            ClassAverage = Math.Round(ClassAverage, 2);
+
+            ClassAverage = SumOfMarks / StudentCounter; //Calculating the class average
+            ClassAverage = Math.Round(ClassAverage, 2); //Rounding the class average to 2 decimal places
             Console.WriteLine($"Class Average: {ClassAverage}");
 
         }
@@ -197,10 +218,11 @@ namespace SimpleStudentManagementProject_CSharpProject1
 
         static void FindTopPerformingStudent()
         {
-
+            //Declearing Variables
             double top = 0;
             int index = 0;
 
+            //Finding the top performing student
             for (int i = 0; i < StudentCounter; i++)
             {
                 if (marks[i] > top)
@@ -222,18 +244,21 @@ namespace SimpleStudentManagementProject_CSharpProject1
 
         static void SortStudentsByMarks()
         {
+            //Sorting the students by marks
             for (int i = 0; i < StudentCounter; i++)
             {
-                for (int j = i + 1; j < StudentCounter; j++)
+                
+                for (int j = i + 1; j < StudentCounter; j++) //compare 1st element with 2nd element
                 {
+                    //Declearing Variables
                     double tempMark = marks[i];
                     string tempName = names[i];
                     int tempAge = Ages[i];
                     DateTime tempDate = dates[i];
 
 
-
-                    if (marks[i] < marks[j]) //compare 2nd element with 1st element 
+                    //Swapping the elements
+                    if (marks[i] < marks[j]) 
                     {
 
                         marks[i] = marks[j];
@@ -256,6 +281,8 @@ namespace SimpleStudentManagementProject_CSharpProject1
             }
 
             Console.WriteLine("*------  Students Sorted by Marks  ------*");
+
+            //Printing the sorted students
             for (int i = 0; i < StudentCounter; i++)
             {
                 Console.WriteLine($"{i}: Name of Student: {names[i]} , Student Age: {Ages[i]} , Student Mark: {marks[i]} , Student Enrollment Date: {dates[i]} \n");
@@ -268,9 +295,11 @@ namespace SimpleStudentManagementProject_CSharpProject1
 
         static void DeleteStudentRecord()
         {
+            //Declearing Variables
             int index;
             string StudentName;
 
+            //Deleting the student record
             for (int i = 0; i < StudentCounter; i++)
             {
                 Console.WriteLine("Enter Student Name:");
@@ -280,10 +309,11 @@ namespace SimpleStudentManagementProject_CSharpProject1
                 
                 index = Array.IndexOf(names, StudentName);
 
+               
                 if (index != -1)
                 {
                     
-                    for (int j = index; j < StudentCounter - 1; j++)
+                    for (int j = index; j < StudentCounter - 1; j++) //Shifting the elements
                     {
                         names[j] = names[j + 1];
                         Ages[j] = Ages[j + 1];
@@ -291,10 +321,11 @@ namespace SimpleStudentManagementProject_CSharpProject1
                         dates[j] = dates[j + 1];
                     }
 
-                    StudentCounter--;
+                    StudentCounter--; //Decrementing the counter
 
                     Console.WriteLine("Student Record Deleted Successfully!");
-                    for (int k = 0; k < StudentCounter; k++)
+
+                    for (int k = 0; k < StudentCounter; k++) //Printing the updated students
                     {
                         Console.WriteLine($"{k}: Name of Student: {names[k]} , Student Age: {Ages[k]} , Student Mark: {marks[k]} , Student Enrollment Date: {dates[k]} \n");
                     }
